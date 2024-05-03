cae954c222c255b0fadf4c2b2fb24ad1c6f7a82d
Daniil-l17 <mlumed221@gmail.com>
2024-05-03 22:00:58 +0500

    добавил swiper

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,10 +1,26 @@
 import style from './project.module.scss'
-
+import 'swiper/css'
+import 'swiper/css/scrollbar'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import {Scrollbar} from 'swiper/modules'
 export const Project = () => {
   return (
-    <div id='project' className={style.project}>
-      <div className=' w-auto pt-20 m-auto flex justify-center items-center'>
-        <h2>Мои проекты</h2>
+    <div id="project" className={style.project}>
+      <div className="flex-col w-full pt-20 max-w-[1400px] m-auto flex  justify-center items-center">
+        <h2 className=" mb-6 text-4xl font-medium">Мои проекты</h2>
+        <Swiper
+          scrollbar={{
+            hide: true,
+          }}
+          modules={[Scrollbar]}
+          className="mySwiper h-[620px] w-full "
+        >
+          <SwiperSlide className="  bg-[#333] !h-[97%]">Slide 1</SwiperSlide>
+          <SwiperSlide className="  bg-[#333] !h-[97%]">Slide 2</SwiperSlide>
+          <SwiperSlide className="  bg-[#333] !h-[97%]">Slide 3</SwiperSlide>
+          <SwiperSlide className="  bg-[#333] !h-[97%]">Slide 4</SwiperSlide>
+          <SwiperSlide className="  bg-[#333] !h-[97%]">Slide 5</SwiperSlide>
+        </Swiper>
       </div>
     </div>
   )
